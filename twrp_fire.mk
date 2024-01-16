5f214a71d8ece0488c1a71736e8f99805446a053
AntarcticShaurant <109678650+AntarcticShaurant@users.noreply.github.com>
2024-01-16 18:02:34 +0100

    Update twrp_fire.mk

--- twrp_fire.mk
+++ twrp_fire.mk
@@ -9,8 +9,8 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
 
-# Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+# Inherit some common TWRP stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from fire device
 $(call inherit-product, device/xiaomi/fire/device.mk)
