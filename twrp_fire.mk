c67c567c88f1756096bb00182ea2132385b052c7
shub876 <8706shubham@gmail.com>
2024-01-23 12:09:16 +0530

    twrp_RMX2001.mk: Cleanup

--- twrp_fire.mk
+++ twrp_fire.mk
@@ -7,9 +7,10 @@
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 
-# Inherit some common twrp stuff.
+# Inherit some common Twrp stuff.
 $(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from fire device
@@ -18,12 +19,5 @@ $(call inherit-product, device/xiaomi/fire/device.mk)
 PRODUCT_DEVICE := fire
 PRODUCT_NAME := twrp_fire
 PRODUCT_BRAND := Redmi
-PRODUCT_MODEL := fire
+PRODUCT_MODEL := Redmi 12
 PRODUCT_MANUFACTURER := xiaomi
-
-PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
-
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="vnd_fire-user 12 TP1A.220624.014 V14.0.4.0.TMXINXM release-keys"
-
-BUILD_FINGERPRINT := Redmi/vnd_fire/fire:12/TP1A.220624.014/V14.0.4.0.TMXINXM:user/release-keys
