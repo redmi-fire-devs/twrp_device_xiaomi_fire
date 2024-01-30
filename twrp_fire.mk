895ce7df2f068e7057a45f280bb15105dd64828c
AntarcticShaurant <109678650+AntarcticShaurant@users.noreply.github.com>
2024-01-30 10:22:16 +0100

    Import Shub's commits

--- twrp_fire.mk
+++ twrp_fire.mk
@@ -7,7 +7,8 @@
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 
 # Inherit some common TWRP stuff.
 $(call inherit-product, vendor/twrp/config/common.mk)
@@ -18,12 +19,5 @@ $(call inherit-product, device/xiaomi/fire/device.mk)
 PRODUCT_DEVICE := fire
 PRODUCT_NAME := twrp_fire
 PRODUCT_BRAND := Redmi
-PRODUCT_MODEL := fire
+PRODUCT_MODEL := Redmi 12
 PRODUCT_MANUFACTURER := xiaomi
-
-PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
-
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="vnd_fire-user 12 TP1A.220624.014 V14.0.13.0.TMXMIXM release-keys"
-
-BUILD_FINGERPRINT := Redmi/vnd_fire/fire:12/TP1A.220624.014/V14.0.13.0.TMXMIXM:user/release-keys
