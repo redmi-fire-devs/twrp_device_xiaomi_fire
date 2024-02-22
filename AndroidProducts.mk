6164ebe532c4408e37f2f740169356ff2425d642
AntarcticShaurant <109678650+AntarcticShaurant@users.noreply.github.com>
2024-02-22 09:25:37 +0100

    Sync branch (#6)
    * Update README.md
    
    * Fix status bar
    
    * Fix notch bar height
    
    * Shrink notch bar height
    
    * Add TW_INCLUDE_FASTBOOTD
    
    * Fix zip install bug
    
    * Update ueventd.mt6768.rc + remove leds
    
    * Trying to fix recovery zip flashing issue
    
    * Revert Notchbar
    
    It seems that people prefer having no notchbar, so i might add it on a different build
    
    * Test twrpfastboot=0
    
    Testing whether this makes flashing stock boot image unneeded
    
    * Update CPU variant
    
    * Code clean up + moved and imported some blobs (#5)
    
    * init.recovery.usb.rc clean up
    
    * Add added libimsg_log.so
    
    * BoardConfig.mk clean up
    
    * Try fixing flash zip bug
    
    * Try fixing flash zip issue
    
    * Remove offset
    
    * Remove offset
    
    Thought i removed both the offsets
    
    * Delete empty twrp.dependencies
    
    * Clean up + Add AVB
    
    * Clean up
    
    * Clean up
    
    * Update README.md
    
    * README.md Clean up
    
    * Update README.md
    
    * Remove maintainer flag
    
    Since the version was too long

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,12 +1,11 @@
 #
-# Copyright (C) 2023 The Android Open Source Project
-# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
-PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/twrp_fire.mk
+PRODUCT_MAKEFILES := $(LOCAL_DIR)/twrp_fire.mk
 
 COMMON_LUNCH_CHOICES := twrp_fire-eng
     
